d6466197109489e9f64734ab7ac291f09c5141dd
shubham2128 <shubham.singh280795@gmail.com>
2024-04-25 20:21:24 +0530

    wsrf changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-0#k1c6s6rhmj+%g6)3_w2101(-m^8=9&$vwk3d5gnex!ub!#^l'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # ALLOWED_HOSTS = ['djangoecom.azurewebsites.net']
 ALLOWED_HOSTS = ['*']
 
