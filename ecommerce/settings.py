1641d60f16b3602bc5e1f0e3dda738bc1d250f7d
shubham2128 <shubham.singh280795@gmail.com>
2024-04-25 19:16:37 +0530

    Update allowed host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0#k1c6s6rhmj+%g6)3_w2101(-m^8=9&$vwk3d5gnex!ub!#^l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoecom.azurewebsites.net']
 
 # Application definition
 INSTALLED_APPS = [
